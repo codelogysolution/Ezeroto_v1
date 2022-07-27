1e49e37bc048a8a9741a867f56e80e4a213ad4a4
VelkataIvanov <velkataivanov@gmail.com>
2022-07-27 17:16:32 +0300

    First commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// Menu button 
+const menu_btn = document.querySelector('.menu-btn');
+const header =document.querySelector('.header');
+
+menu_btn.addEventListener('click', function(){
+    menu_btn.classList.toggle('active');
+    header.classList.toggle('active');
+});
+
+// Image swipper 
+
+
